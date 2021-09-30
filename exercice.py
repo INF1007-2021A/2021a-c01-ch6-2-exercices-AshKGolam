1746e10357326aa785cafccebc5cec786b676b3b
AshKGolam <90001285+AshKGolam@users.noreply.github.com>
2021-09-30 14:06:07 -0400

    ok

--- exercice.py
+++ exercice.py
@@ -6,26 +6,39 @@ from matplotlib.colors import cnames
 
 def list_to_dict(some_list: list) -> dict:
     # TODO: Transformer la liste en dictionnaire, les éléments de la liste deviennent les clés et leur index deviennent les valeurs
-
-    return {}
+    index = 0
+    my_dict = {}
+    for each in some_list:
+        my_dict[index] = each
+        index += 1
+    return my_dict
 
 
 def color_name_to_hex(colors: list) -> list:
     # TODO: Trouver la valeur hex de chaque couleur dans la liste et créer une liste de tupple où le premier élément est le nom de la couleur et le deuxième est la valeur hex
-
-    return []
+    my_list = []
+    my_list = [(each,cnames[each]) for each in colors if each in cnames]
+    return my_list
 
 
 def create_list() -> list:
     # TODO: Créer une liste des 10 000 premiers entiers positif, sauf pour les entiers de 15 à 350
-
-    return []
+    my_list = []
+    my_list= [i for i in range(0,10001) if i < 15 or i > 350]
+    return my_list
 
 
 def compute_mse(model_dict: dict) -> dict:
     # TODO: Calculer l'erreur quadratique moyen pour chaque modèle. Retourner un dictionnaire contenant les MSE.
-
-    return {}
+    my_dict = {}
+    for key in model_dict:
+        mse = 0
+        n = 1
+        for tup in model_dict[key]:
+           mse = mse + (1/n)*(tup[0]-tup[1])**2
+           n = n + 1
+        my_dict[key] = mse
+    return my_dict
 
 
 def main() -> None:
